3b7169190cd865e3859c4530165770c8724a2d20
jonlindholm11 <jonlindholm11@gmail.com>
2025-10-02 21:50:50 -0400

    seeding special pricing

---
+++ db/seed/special_pricing_seed.js
@@ -0,0 +1,18 @@
+import db from "../client.js";
+
+await db.connect();
+await seedRoles();
+await db.end();
+console.log("🌱 Roles seeded.");
+
+async function seedSpecialPricing() {
+    const specialPricing = {
+        product_id: 1,
+        special_price: 15.99,
+        start_date: "2025-10-30T:10:00:00Z",
+        end_date: "2025-11-15T:10:00:00Z",
+        is_active: true,
+        created_by_user_id: 1
+    }
+    return specialPricing
+}
