751558fd6524fea8d08291e5a997c31adeabe9d9
jonlindholm11 <jonlindholm11@gmail.com>
2025-10-08 21:30:58 -0400

    customers querys

---
+++ db/queries/customers.js
@@ -0,0 +1,52 @@
+import db from "#db/client";
+
+export async function createCustomers(
+  user_id,
+  company_name,
+  contact_name,
+  price_tier_id,
+  assigned_salesman_id
+) {
+  const SQL = `
+    INSERT INTO customers (user_id, company_name, contact_name, price_tier_id, assigned_salesman_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+    `;
+  const { rows: customers } = await db.query(
+    SQL,
+    (user_id, company_name, contact_name, price_tier_id, assigned_salesman_id)
+  );
+  return customers;
+}
+
+export async function getCustomerByUser_Id(user_id) {
+  const SQL = `
+    SELECT * FROM customers WHERE user_id = $1
+    `;
+  const { rows: customer_id } = await db.query(SQL, [user_id]);
+  return customer_id;
+}
+
+export async function getCustomerByCompany_Name(company_name) {
+  const SQL = `
+    SELECT * FROM customers WHERE company_name = $1
+    `;
+  const { rows: customer_company } = await db.query(company_name);
+  return customer_company;
+}
+
+export async function getCustomerByContact_Name(contact_name) {
+  const SQL = `
+    SELECT * FROM customers WHERE contact_name = $1
+    `;
+  const { rows: customer_contact } = await db.query(contact_name);
+  return customer_contact;
+}
+
+export async function getCustomerByAssigned_Salesman_Id(assigned_salesman_id) {
+  const SQL = `
+    SELECT * FROM customers WHERE assigned_salesman_id = $1
+    `;
+  const { rows: customer_salesman } = await db.query(assigned_salesman_id);
+  return customer_salesman;
+}
