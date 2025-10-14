3e9b0f75465ef978fa021176932c851019df372f
jonlindholm11 <jonlindholm11@gmail.com>
2025-10-14 19:25:31 -0400

    get all and get by id orders items router

---
+++ api/order_items.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getOrder_Items, getOrder_ItemsById } from "../db/queries/orders.js";
+
+const router = express.Router();
+export default router;
+
+router.route("/products").get(async (req, res) => {
+  const orders_items = await getOrder_Items(req.orders);
+  res.send(orders_items);
+});
+
+router.route("/products/:id").get(async (req, res) => {
+  const orders_itemsById = await getOrder_ItemsById(req.orders.id);
+  res.send(orders_itemsById);
+});
